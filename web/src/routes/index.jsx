a954d3654571495efed3dc1717f7e181aaebaa7f
GeovaneMT <gmtgeovane@gmail.com>
2024-03-23 16:57:33 -0300

    ready

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,44 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from '../utils/roles';
 
-import { AppRoutes } from './app.routes';
+import { CustomerRoutes } from './customer.routes';
+import { AdminRoutes } from './admin.routes';
+import { SaleRoutes } from './sale.routes';
 import { AuthRoutes } from './auth.routes';
+import { useEffect } from 'react';
+import { api } from '../services/api';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api
+      .get('/users/validated')
+      .catch((error) => {
+        if (error.response.status === 401) {
+          signOut()
+        }
+      })
+  }, [])
+
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />
+      case USER_ROLE.SALE:
+        return <SaleRoutes />
+      default:
+        return <CustomerRoutes />
+    }
+  }
 
   return (
     <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
